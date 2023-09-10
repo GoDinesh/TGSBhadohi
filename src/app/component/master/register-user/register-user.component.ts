15d017d02a6a022d03ef0a9280062a8932d528b0
GoDinesh <dinesh03081987@gmail.com>
2023-09-10 19:12:43 +0530

    User Registration

--- src/app/component/master/register-user/register-user.component.ts
+++ src/app/component/master/register-user/register-user.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { msgTypes } from 'src/app/constants/common/msgType';
+import { User } from 'src/app/model/master/user.model';
+import { SweetAlertService } from 'src/app/service/common/sweet-alert.service';
 import { ValidationErrorMessageService } from 'src/app/service/common/validation-error-message.service';
+import { UserService } from 'src/app/service/masters/user.service';
 import { CustomValidation } from 'src/app/validators/customValidation';
 
 @Component({
@@ -10,28 +15,42 @@ import { CustomValidation } from 'src/app/validators/customValidation';
 })
 export class RegisterUserComponent {
 
+  userModel: User = new User();
+  posts:User[]=[];
+  dtOptions: any = {};
+  actionFlag: boolean = true;
+
   formGroup = new FormGroup({
+    id: new FormControl(),
     role: new FormControl(),
     email: new FormControl(),
     password: new FormControl(),
     confirmPassword: new FormControl(),
+    active: new FormControl()
 })
 
 constructor(private formBuilder: FormBuilder,
   public validationMsg: ValidationErrorMessageService,
+  private userService: UserService,
+  private alerService: SweetAlertService
   ){
 }
 
 ngOnInit(){
-this.createForm();
+this.createForm(new User());
+this.loadTable();
+this.getTableRecord();
+
 }
 
-createForm() {
+createForm(usermodel: User) {
   this.formGroup = this.formBuilder.group({
-    role: ['',[Validators.required]],
-    email: ['',[Validators.required, CustomValidation.emailId]],
-    password: ['',[Validators.required, CustomValidation.password]],
-    confirmPassword: ['',[Validators.required]],
+    id: [usermodel.id],
+    role: [usermodel.role,[Validators.required]],
+    email: [usermodel.email,[Validators.required, CustomValidation.emailId]],
+    password: [usermodel.password,[Validators.required, CustomValidation.password]],
+    confirmPassword: [usermodel.confirmPassword,[Validators.required, CustomValidation.password]],
+    active: [usermodel.active]
   },{ validator: CustomValidation.confirmedValidator('password', 'confirmPassword') });
 }
 
@@ -39,12 +58,70 @@ get formControll(){
   return this.formGroup.controls;
 }
 
-  register(){
+getTableRecord(){
+  this.userService.getAllUsers().subscribe(res=>{
+    if(res.status === msgTypes.SUCCESS_MESSAGE){
+      this.posts = res.data;
+    }
+});
+}
 
+
+//load the table
+loadTable(){
+  this.dtOptions = {
+    processing: true,
+    scrollY: "300px",
+    scrollCollapse: true,
+    dom: '<"align-table-buttons"Bf>rt<"bottom align-table-buttons"><"clear">',
+    buttons: [
+      'copy', 'csv', 'excel', 'print'
+    ]
+  };
+}
+
+register(){
+  this.userModel = {...this.userModel,...this.formGroup.value}
+  try{
+          this.userService.insertUser(this.userModel).subscribe(res=>{
+            if(res.status === msgTypes.SUCCESS_MESSAGE)
+            this.getTableRecord();
+            this.resetForm();
+          });
+    }catch(error){}  
+}
+
+resetForm(){
+
+}
+
+//change the status
+async slideToggleChange(element: MatSlideToggleChange, data: User) {
+  const flag = await this.alerService.updateAlert()
+  if(flag)  {
+        data.active = !data.active;
+        this.userService.insertUser(data).subscribe();
+  }else{
+        element.source.checked = data.active;
   }
+}
 
-  resetForm(){
+//set value in formfield to update
+setVlaueToUpdate(data:User){
+    data.confirmPassword = data.password;
+    this.createForm(data);
+    this.actionFlag = false;
+}
 
-  }
+//update the record
+update(){
+    this.userModel = {...this.userModel,...this.formGroup.value}
+    this.userService.insertUser(this.userModel).subscribe((res)=>{
+      if(res.status === msgTypes.SUCCESS_MESSAGE){
+        this.getTableRecord();
+        this.resetForm();
+      }
+    });
+} 
 
 }
---
+++ src/app/service/masters/user.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { appurl } from 'src/app/constants/common/appurl';
+import { AcademicYear } from 'src/app/model/master/academic-year.model';
+import { User } from 'src/app/model/master/user.model';
+import { ResponseModel } from 'src/app/model/shared/response-model.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  requestUrl = appurl.menuurl_master + appurl.master_user;
+  constructor(private httpClient: HttpClient) { }
+ 
+  insertUser(user: User){
+     const url = this.requestUrl + appurl.endpoint_insert;
+     return this.httpClient.post<ResponseModel>(url, JSON.stringify(user))
+  }
+  
+  getAllUsers(){
+    const url = this.requestUrl + appurl.endpoint_findall;
+    return this.httpClient.get<ResponseModel>(url)
+  }
+}
